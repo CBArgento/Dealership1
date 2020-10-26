0222525cb5cea3b88494cc19e6fd4a081c9f24a0
Lucas Di Fini <difinile@gmail.com>
2020-10-26 16:02:10 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,5 @@
 // JavaScript source code
+/* Live chat popup window function */
+function liveChat() {
+    console.log('Popping up the live chat window');       // Popup the live chat window code here.
+}
